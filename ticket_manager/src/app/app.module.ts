eb7c5db2ddabb6a4832d512824791dfd8f7614b5
ltessewamba <loica-cynthiche.tesse-wamba@etudiant.univ-rennes1.fr>
2024-04-24 17:17:59 +0100

    routing

--- ticket_manager/src/app/app.module.ts
+++ ticket_manager/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UsersComponent } from './users/users.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
+import { ProjectListingComponent } from './projects/project-listing/project-listing.component';
+import { TicketDetailComponent } from './tickets/ticket-detail/ticket-detail.component';
+import { TicketListingComponent } from './tickets/ticket-listing/ticket-listing.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { UserListingComponent } from './users/user-listing/user-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    UsersComponent,
+    TicketsComponent,
+    ProjectDetailComponent,
+    ProjectListingComponent,
+    TicketDetailComponent,
+    TicketListingComponent,
+    UserDetailComponent,
+    UserListingComponent
   ],
   imports: [
     BrowserModule,
